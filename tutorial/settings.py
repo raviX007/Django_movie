73250727055df0bf9a883d9a87ef0392bfe2d56c
raviX007 <91694681+raviX007@users.noreply.github.com>
2024-04-06 01:14:31 +0530

    new customizations

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--b9qcc7m$=ci**8e$kd9c-9m%u673m^fg3=23=*erx&tn=bneq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -87,6 +87,7 @@ DATABASES = {
 }
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -122,6 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
